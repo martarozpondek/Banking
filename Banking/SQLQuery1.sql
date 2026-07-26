7f331f75815c2f54d5a2ffbe3f793d81b54e8ca5
martarozpondek <martarozpondek@wp.pl>
2026-07-26 22:10:10 +0200

    Create some new tables

--- Banking/SQLQuery1.sql
+++ Banking/SQLQuery1.sql
@@ -94,7 +94,7 @@ CREATE TABLE Account
 	CurrentBalance DECIMAL(18,2) NOT NULL CONSTRAINT DF_Account_CurrentBalance DEFAULT (0),
 	AvailableBalance DECIMAL(18,2) NOT NULL CONSTRAINT DF_Account_AvailableBalance DEFAULT (0),
 	CreatedAt DATETIMEOFFSET(0) NOT NULL CONSTRAINT DF_Account_CreatedAt DEFAULT SYSDATETIMEOFFSET(),
-	
+
 	CONSTRAINT PK_Account PRIMARY KEY (AccountId),
 	CONSTRAINT FK_Customer FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
 	CONSTRAINT FK_Account_Currency FOREIGN KEY (CurrencyCode) REFERENCES Currency(CurrencyCode),
@@ -195,3 +195,91 @@ CREATE TABLE OverdraftInterest
 	CONSTRAINT FK_OverdraftInterest_TransactionStatus FOREIGN KEY (TransactionStatusId) REFERENCES TransactionStatus(TransactionStatusId)
 
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE CardStatus
+(
+	CardStatusId BIGINT IDENTITY(1,1) NOT NULL,
+	Code VARCHAR(30) NOT NULL, 
+    Name NVARCHAR(100) NOT NULL,
+
+	CONSTRAINT PK_CardStatus PRIMARY KEY (CardStatusId),
+	CONSTRAINT UQ_CardStatus_Code UNIQUE (Code),
+)
+CREATE TABLE Card
+(
+	CardId BIGINT IDENTITY(1,1) NOT NULL,
+	CustomerId UNIQUEIDENTIFIER NOT NULL,
+	AccountId BIGINT NOT NULL,
+	CardHolderName NVARCHAR(100) NOT NULL,
+	CardNumberHash VARBINARY(64) NOT NULL, /* Stores a cryptographic hash of the card number */
+	LastFourDigits CHAR(4) NOT NULL,
+	CreatedAt DATETIMEOFFSET(0) NOT NULL CONSTRAINT DF_Card_CreatedAt DEFAULT SYSDATETIMEOFFSET(),  /*created at system */
+	IssuedAt DATE NOT NULL, /* Date when the card was issued to the customer */
+	CardStatusId BIGINT NOT NULL,
+	ActivatedAt Date NULL,
+	ExpiresAt DATE NOT NULL,
+	
+
+
+	CONSTRAINT PK_Card PRIMARY KEY (CardId),
+	CONSTRAINT FK_Card_Customer FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
+	CONSTRAINT FK_Card_Account FOREIGN KEY (AccountId) REFERENCES Account(AccountId),
+	CONSTRAINT FK_CardStatus_Card FOREIGN KEY (CardStatusId) REFERENCES CardStatus(CardStatusId), 
+	CONSTRAINT UQ_Card_CardNumberHash UNIQUE(CardNumberHash),
+	CONSTRAINT CK_Card_LastFourDigits CHECK(LastFourDigits NOT LIKE '%[^0-9]%' AND LEN(LastFourDigits)=4)
+);
+
+CREATE TABLE CardSecurity
+(
+	CardAuthorizationId BIGINT IDENTITY(1,1) NOT NULL,
+	CardId BIGINT NOT NULL CONSTRAINT UQ_CardSecurity_Card UNIQUE(CardId),
+	PinRetryCount INT NOT NULL DEFAULT(0),
+	BlockedUntil DATETIMEOFFSET(0) NULL,
+	IsBlocked BIT NOT NULL DEFAULT(0),
+	PinChangedAt DATETIMEOFFSET(0) NULL,
+	CreatedAt DATETIMEOFFSET(0) NOT NULL CONSTRAINT DF_CardSecurity_CreatedAt DEFAULT SYSDATETIMEOFFSET(),
+	UpdatedAt DATETIMEOFFSET(0) NULL,
+
+	CONSTRAINT PK_CardAuthorization PRIMARY KEY (CardAuthorizationId),
+	CONSTRAINT FK_CardAuthorization FOREIGN KEY (CardId) REFERENCES Card(CardId),
+	CONSTRAINT CK_CardSecurity_PinRetryCountCHECK(PinRetryCount BETWEEN 0 AND 3) /*PIN entry limit: 0–3 attempts */
+);
+/*
+CREATE TABLE CardTransactionAuthorizationStatus
+(
+	CardTransactionAuthorizationStatusId INT IDENTITY(1,1) NOT NULL,
+	Code NOT NULL
+);
+CREATE TABLE CardTransactionAuthorization
+(
+	CardTransactionAuthorizationId BIGINT IDENTITY(1,1) NOT NULL,
+	MerchantId INT NOT NULL,
+	MCCCode CHAR(4) NOT NULL,
+
+	CONSTRAINT PK_CardTransactionAuthorization PRIMARY KEY (CardTransactionAuthorizationId),
+	CONSTRAINT FK_CardTransactionAuthorization_Merchant FOREIGN KEY (MerchantId) REFERENCES Merchant(MerchantId),
+	CONSTRAINT FK_CardTransactionAuthorization_MCCCode FOREIGN KEY (MCCCode) REFERENCES MerchantCategoryCode(MCCCode),
+);
+*/
